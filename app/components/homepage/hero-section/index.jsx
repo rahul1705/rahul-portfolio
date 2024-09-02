95ab2b01bccd3fb4fa3ec2455a70c74eb960c6ea
Rahul Mandal <rmandal1705@gmail.com>
2024-09-02 23:40:49 +0530

    final  commit

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -112,7 +112,7 @@ function HeroSection() {
               <div className="ml-4 lg:ml-8 mr-2">
                 <span className=" text-white">skills:</span>
                 <span className="text-gray-400">{`[`}</span>
-                <span className="text-amber-300">'{skillsData.join("', '")}'</span>
+                <span className="text-amber-300">{`'${skillsData.join("', '")}'`}</span>
                 <span className="text-gray-400">{"],"}</span>
               </div>
               <div>
